872f9b07020fce876bbcc919eb47f457546ec675
xieyyt <xieyyt@yonyou.com>
2019-12-19 17:27:45 +0800

    Initial commit

---
+++ src/main/java/com/xieyy/sso/XieyySsoApplication.java
@@ -0,0 +1,13 @@
+package com.xieyy.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XieyySsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XieyySsoApplication.class, args);
+    }
+
+}
